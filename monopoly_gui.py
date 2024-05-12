09c0812da8866588f44a99cbdd6f511a1363e3c3
yehchiayu <107289388+yehchiayu@users.noreply.github.com>
2024-05-12 21:05:41 +0800

    second
    add print

--- monopoly_gui.py
+++ monopoly_gui.py
@@ -25,6 +25,6 @@ canvas.create_image(480, 240, image=bg_image)
 # 建立遊戲開始按鈕
 start_button = tk.Button(root, text="開始遊戲", command=select_character, bg="blue", fg="white", font=("Arial", 12, "bold"), bd=3, relief=tk.RAISED)
 start_button.place(x=450, y=350)
-
+print()
 # 執行主迴圈
 root.mainloop()
