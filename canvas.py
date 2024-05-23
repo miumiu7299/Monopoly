62a2a93b89abf5e476dff4d23b98066c24a96df5
yehchiayu <107289388+yehchiayu@users.noreply.github.com>
2024-05-23 08:50:58 +0800

    revise board cuisine
    revise board cuisine

--- canvas.py
+++ canvas.py
@@ -43,68 +43,64 @@ class MonopolyGame:
     def __init__(self, ui):
         self.players = []
         self.current_turn = 0
-        self.board_size = 28 #共30格
+        self.board_size = 26 #共30格
         self.properties = self.create_properties()
         self.ui = ui
 
     def create_properties(self):
         properties = []
         for i in range(self.board_size):
-            if i in [3, 18]: 
+            if i in [4, 16]: 
                 properties.append(Property(f"Chance or Destiny{i}", type="chanceordestiny"))
-            elif i in [23]: 
+            elif i in [19]: 
                 properties.append(Property(f"Emergency {i}", type="emergency"))#直接去醫院
-            elif i in [21]: 
+            elif i in [22]: 
                 properties.append(Property(f"Fat->killed {i}", type="fattokilled"))
-            elif i in [14]: 
+            elif i in [13]: 
                 properties.append(Property(f"Hospital {i}", type="hospital"))
-            elif i in [9]: 
+            elif i in [10]: 
                 properties.append(Property(f"Magic Card {i}", type="magiccard"))
-            elif i in [6]: 
+            elif i in [9]: 
                 properties.append(Property(f"Jail {i}", type="jail"))
         
-            elif i in [27]: 
+            elif i in [25]: 
                 properties.append(Property(f"媽媽的愛 {i}", 4500))
-            elif i in [26]: 
-                properties.append(Property(f"A5和牛 {i}", 3000))
                 
             elif i in [24]: 
-                properties.append(Property(f"魚子醬 {i}", 1500))
+                properties.append(Property(f"A5和牛 {i}", 3000))
             elif i in [23]: 
-                properties.append(Property(f"松葉蟹 {i}", 3500))
-            elif i in [22]: 
+                properties.append(Property(f"魚子醬 {i}", 1500))
+            elif i in [21]: 
                 properties.append(Property(f"鮑魚烏參佛跳牆 {i}", 2200))
             elif i in [20]: 
                 properties.append(Property(f"威靈頓牛排 {i}", 2800))
-            elif i in [19]: 
-                properties.append(Property(f"龍蝦 {i}", 1000))
+            elif i in [18]: 
+                properties.append(Property(f"龍蝦 {i}", 100))
             elif i in [17]: 
-                properties.append(Property(f"火鍋{i}", 350))
-            elif i in [16]: 
-                properties.append(Property(f"義大利麵 {i}", 400))
+                properties.append(Property(f"牛排 {i}", 500))
             elif i in [15]: 
-                properties.append(Property(f"牛排 {i}", 700))
-            elif i in [13]: 
+                properties.append(Property(f"火鍋{i}", 350))
+            elif i in [14]: 
                 properties.append(Property(f"pizza {i}", 300))
             elif i in [12]: 
-                properties.append(Property(f"壽司 {i}", 350))
+                properties.append(Property(f"義大利麵 {i}", 400))
             elif i in [11]: 
                 properties.append(Property(f"燒烤 {i}", 800))
-            elif i in [10]: 
-                properties.append(Property(f"石鍋拌飯 {i}", 200))
             elif i in [8]: 
-                properties.append(Property(f"牛肉麵 {i}", 250))
+                properties.append(Property(f"石鍋拌飯 {i}", 200))
             elif i in [7]: 
-                properties.append(Property(f"咖哩 {i}", 100))
+                properties.append(Property(f"牛肉麵 {i}", 250))
+            elif i in [6]: 
+                properties.append(Property(f"壽司 {i}", 350))
             elif i in [5]: 
-                properties.append(Property(f"新竹人的❤️ 麥當勞 {i}", 150))
-            elif i in [4]: 
-                properties.append(Property(f"便當 {i}", 80))
+                properties.append(Property(f"咖哩 {i}", 100))
+            elif i in [3]: 
+                properties.append(Property(f"韓式炸雞 {i}", 250))
             elif i in [2]: 
-                properties.append(Property(f"想不到吃什麼 7-11 {i}", 70))
+                properties.append(Property(f"新竹人的❤️ 麥當勞 {i}", 150))
             elif i in [1]: 
-                properties.append(Property(f"不健康的泡麵{i}", 50))
-            else:
+                properties.append(Property(f"便當 {i}", 80))
+            elif i in [0]: 
                 properties.append(Property(f"Start {i}", 0))
         return properties
 
@@ -163,7 +159,7 @@ class MonopolyGame:
             elif property.type == "emergency":
                 self.ui.add_message(f"{player.name}  landed on a Emergency space and moved to hospital.")
                 player.is_emergency = True
-                player.position=16
+                player.position=13
             elif property.type == "fattokilled":
                 self.ui.add_message(f"{player.name} landed on a Fat->killed space , you are so fat that you will get killed!!!")
             elif property.type == "hospital":
@@ -270,7 +266,8 @@ class MonopolyUI:
         self.player_texts = []
         positions = [(0.01, 0.5), (0.99, 0.5), (0.01, 0.5), (0.99, 0.5)]
         #positions = [(0.01, 0.01), (0.99, 0.01), (0.01, 0.99), (0.99, 0.99)]
-        anchors = ['nw', 'ne', 'sw', 'se']
+        #anchors = ['nw', 'ne', 'sw', 'se']
+        anchors = [ 'sw', 'se','nw', 'ne']
         for i, (pos, anchor) in enumerate(zip(positions, anchors)):
             frame = tk.Frame(self.main_frame, width=200, height=100)
             frame.place(relx=pos[0], rely=pos[1], anchor=anchor)
